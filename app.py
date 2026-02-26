887118e2b3316563096f7879a69d2d2977df3435
Swajith-ai <ai.swajiths@gmail.com>
2026-02-26 17:24:42 +0630

    Update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.schema import Document
+from langchain_core.documents import Document
 
 from sentence_transformers import CrossEncoder
 from rank_bm25 import BM25Okapi
